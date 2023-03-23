9b7b8df2dc43047ac6346a567bb5eddbb71febfb
Pa1nkil3er <kiril.july@gmail.com>
2023-03-23 17:08:00 +0200

    added practice

--- Task on 21.03/JS/script.js
+++ Task on 21.03/JS/script.js
@@ -25,3 +25,34 @@ let capacity = +prompt("Enter the capacity of your flash drive(in GB)");
 let amount = (1024 * capacity) / 820;
 alert(`This flash drive can store ${amount.toFixed(0)} of 820 mb files `);
 
+//Task 6
+
+const miles = 0.621371
+let km = +prompt("Write how much kms would you want to convert into miles: ")
+let converted = km / miles
+alert(`${km} km in miles will be: ${converted.toFixed(0)} `)
+
+//Task 7
+let hours = +prompt("What is the current hour?")
+let minutes = +prompt("What is the current minute?")
+let hour = 23
+let minute = 60
+let left_hour = hour - hours
+let left_minutes = minute - minutes
+alert(`It is ${left_hour} hours and ${left_minutes} minutes left till the end of the day`)
+
+//Task 8
+
+let digit = +prompt("Write a 3-digit number")
+let a = Math.floor((digit / 100))
+let b = digit % 10
+let c = Math.floor(((digit % 100) / 10))
+let reversed_digit = c.toString() + b.toString() + a.toString()
+alert(`${reversed_digit}`)
+
+
+
+
+
+
+
